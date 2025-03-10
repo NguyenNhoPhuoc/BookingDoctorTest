ae7ba859882a66489258d80ec7cc58636ce92c34
vanlinh1604 <vanlinhdzpro16042003@gmail.com>
2025-03-10 15:52:35 +0700

    fontend update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,14 +8,17 @@ import Home from './pages/Home'
 import Login from './pages/Login'
 import MyAppointments from './pages/MyAppointments'
 import MyProfile from './pages/MyProfile'
+import Navbar from './components/Navbar'
+
 
 const App = () => {
   return (
     <div className='mx-4 sm:mx-[10%]'>
+      <Navbar/>
         <Routes>
             <Route path='/' element={<Home />}/>
             <Route path='/doctors' element={<Doctors />}/>
-            <Route path='/doctors:speciality' element={<Doctors />}/>
+            <Route path='/doctors/:speciality' element={<Doctors />}/>
             <Route path='/login' element={<Login />}/>
             <Route path='/about' element={<About />}/>
             <Route path='/contact' element={<Contact />}/>
--- frontend/src/pages/Doctors.jsx
+++ frontend/src/pages/Doctors.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import { useParams } from 'react-router-dom';
 
 const Doctors = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+    const { speciality } = useParams();
 
-export default Doctors
+    return (
+        <div>
+            <h1>Doctors {speciality ? `- ${speciality}` : ''}</h1>
+        </div>
+    );
+};
+
+export default Doctors;
