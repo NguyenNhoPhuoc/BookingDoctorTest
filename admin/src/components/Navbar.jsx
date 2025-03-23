eb33a7b9aa31acc586e489739893e742b0de624e
Nguyen Nho Phuoc <nguyennhophuoc2003@gmail.com>
2025-03-23 21:41:36 +0700

    update backend and admin screen

---
+++ admin/src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import React, { useContext } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { assets } from '../assets/assets'
+import { AdminContext } from '../context/AdminContext'
+const Navbar = () => {
+    const {aToken,setAtoken} = useContext(AdminContext)
+    const navigate = useNavigate()
+    const logout = () => {
+        navigate('/')
+        aToken && setAtoken('')
+        aToken && localStorage.removeItem('aToken')
+    }
+    return (
+        <div className='flex items-center justify-between px-4 sm:px-10 py-3 border-b border-gray-300 bg-white'>
+            <div className='flex items-center gap-2 text-xs'>
+                <img className='w-36 sm:w-40 cursor-pointer' src={assets.admin_logo} alt="" />
+                <p className='border px-2.5 py-0.5 rounded-full border-gray-500 text-gray-600'>{aToken ? 'Admin' : 'Doctor'}</p>
+            </div>
+            <button onClick={logout} className='bg-[#5F6FFF] px-10 py-2 rounded-full text-sm '>Logout</button>
+        </div>
+    )
+}
+
+
+export default Navbar
--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,14 +1,10 @@
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  resolve: {
-    extensions: ['.js', '.jsx'] 
-  },
   server:{
-    port:5173
+    port:5174
   }
 })
