b5e4bbade75b0f446f96d1b3f89488ff6b7c0e1b
vanlinh1604 <vanlinhdzpro16042003@gmail.com>
2025-03-24 10:48:47 +0700

    update

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Header from '../components/Header'
+import SpecialityMenu from '../components/SpecialityMenu.jsx'
+import TopDoctors from '../components/TopDoctors.jsx'
+import Banner from '../components/Banner.jsx'
 
 const Home = () => {
   return (
     <div>
         <Header/>
+        <SpecialityMenu/>
+        <TopDoctors/>
+        <Banner/>
     </div>
   )
 }
